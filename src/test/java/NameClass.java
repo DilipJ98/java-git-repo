ee066113ca879410de6e1893cadc854aad5670b0
DilipJ98 <dilip@cklabs.in>
2025-07-03 21:51:55 +0530

    add: comment

--- src/test/java/NameClass.java
+++ src/test/java/NameClass.java
@@ -7,5 +7,6 @@ public class NameClass  implements Name{
     public static void main(String[] args) {
         NameClass nameCLass = new NameClass();
         nameCLass.details();
+        //this is from test branch
     }
 }
